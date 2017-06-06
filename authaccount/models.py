698da9efa028a0975ea223a7f4efbf4b43fdf4b8
despean <despean15@gmail.com>
2017-06-06 23:28:32 +0100

    Update models.py

--- authaccount/models.py
+++ authaccount/models.py
@@ -19,21 +19,4 @@ class Profile(models.Model):
     im = get_thumbnail(self.photo, '190x190', crop='center', quality=99)
     return im.url
 
-class Contact(models.Model):
-  user_from = models.ForeignKey(User,
-                                related_name='rel_from_set')
-  user_to = models.ForeignKey(User,
-                              related_name='rel_to_set')
-  created = models.DateTimeField(auto_now_add=True,
-                                 db_index=True)
 
-  class Meta:
-    ordering = ('-created',)
-
-  def __str__(self):
-    return '{} follows {}'.format(self.user_from,
-                                  self.user_to)
-
-
-User.add_to_class('following', models.ManyToManyField('self', through=Contact, related_name='followers',
-                                                      symmetrical=False))
